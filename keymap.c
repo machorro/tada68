fc5d7d195ef70312d281d8800497c54ffa010ec3
Humberto <lizard.machorro@gmail.com>
2018-05-31 16:24:31 -0400

    removed tap dance dot_rt

--- keymap.c
+++ keymap.c
@@ -13,7 +13,6 @@
 //Tap Dance Declarations
 enum {
   COMM_LT = 0, // Comma or Left Angle Bracket (<)
-  DOT_RT, // Dot or Right Angle Bracket (>)
 };
 
 #define _______ KC_TRNS
@@ -36,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
   HYPERCAP, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
-  KC_LSFT,       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   TD(COMM_LT),TD(DOT_RT), KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
+  KC_LSFT,       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   TD(COMM_LT),KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
   KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                        KC_RGUI,KC_RALT,MO(_FL), KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
@@ -65,7 +64,5 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Shift, twice for Caps Lock
   // Tap once add comma, tap twice add <
   [COMM_LT]       = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_LT),
-  // Tap once add comma, tap twice add <
-  [DOT_RT]        = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_GT),
   //Other declarations would go here, separated by commas, if you have them
 };
